2fe728dfa0db042e6e2519a0606b9df06daa985d
Axiom <axiiomatic@users.noreply.github.com>
2024-11-05 19:29:35 -0400

    build home page

--- src/components/Computer/UI/HomeScreen.tsx
+++ src/components/Computer/UI/HomeScreen.tsx
@@ -3,14 +3,35 @@ import {
   CenterWrapper
 } from "@/components/wrappers";
 import { AsciiBanner } from "@/components/ascii";
+import { Link } from "@/lib/i18n";
+import { hexToRGB } from "@/utils/functions";
+import { usePreferences } from "@/utils/contexts";
+
+interface themeColor {
+    textColor: string;
+  }
 
 export const HomeScreen = () => {
+    const { theme } = usePreferences() as { theme: themeColor };
+    const rgb = hexToRGB(theme.textColor);
+    const color = `${rgb.r},${rgb.g},${rgb.b}`
+
     return (
         <CenterWrapper>
-            {AsciiBanner}
+            <div style={{
+                borderBottomColor: `rgba(${color}, 1)`
+            }} className="border-b-[5px] p-5">
+                {AsciiBanner}
+            </div>
 
-            Display
-            CLI
+            <div className={`
+            flex flex-col p-5
+            text-[40px] items-center
+            `}>
+                <Link href="/os/gui">GUI</Link>
+                <Link href="/os/cli">CLI</Link>
+                <Link href="/os/blog">Blog</Link>
+            </div>
         </CenterWrapper>
     )
 };
