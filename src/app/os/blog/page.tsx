9b6802e84678466178d69d416872a0e8572b1f8a
Axiom <axiiomatic@users.noreply.github.com>
2024-11-06 16:51:46 -0400

    added blog page

---
+++ src/app/os/blog/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useEffect } from "react";
+import { MonitorWrapper } from "@/components/wrappers"
+import { usePageHistory, useComputer } from "@/utils/contexts";
+import { BlogScreen } from "@/components/Computer/UI";
+
+export default function GUI() {
+  const { updatePageHistory } = usePageHistory();
+  const { setState } = useComputer();
+
+  useEffect(() => {
+    updatePageHistory("/os/blog");
+    setState("blog");
+  }, [])
+
+  return (
+    <MonitorWrapper>
+      <BlogScreen />
+    </MonitorWrapper>
+  );
+}
---
+++ src/components/Computer/UI/BlogScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  CenterWrapper
+} from "@/components/wrappers";
+
+export const BlogScreen = () => {
+    return (
+        <CenterWrapper>
+            Blog (Work in Progress)
+        </CenterWrapper>
+    )
+};
+  
+export default BlogScreen;
