d598856aeef6b06e2c301298636ae1f459612839
Axiom <axiiomatic@users.noreply.github.com>
2024-11-06 16:52:02 -0400

    added blog to export

--- src/components/Computer/UI/index.ts
+++ src/components/Computer/UI/index.ts
@@ -3,11 +3,13 @@ import LoadingScreen from "./LoadingScreen";
 import HomeScreen from "./HomeScreen";
 import CLIScreen from "./CLIScreen";
 import GUIScreen from "./GUIScreen";
+import BlogScreen from "./BlogScreen";
 
 export {
     TitleScreen,
     LoadingScreen,
     HomeScreen,
     CLIScreen,
-    GUIScreen
+    GUIScreen,
+    BlogScreen
 }
