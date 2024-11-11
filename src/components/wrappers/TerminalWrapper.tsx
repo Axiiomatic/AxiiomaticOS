35c6369ae47152c3d3dacc52d3787561a4d0fccd
Axiom <101472184+Axiiomatic@users.noreply.github.com>
2024-11-11 14:21:54 -0500

    fixed wrapper overlapping with navbar

--- src/components/wrappers/TerminalWrapper.tsx
+++ src/components/wrappers/TerminalWrapper.tsx
@@ -12,7 +12,7 @@ export const TerminalWrapper = ({  className, children, ...props }: Props) => {
         className={`
           text-terminal shadow-0_0_10px_val(--color),0_0_20px_val(--color)
           flex flex-col overflow-y-auto p-[10px] flex-grow
-          font-vt323 z-[10] leading-[1.6]
+          font-vt323 z-[10] leading-[1.6] h-32
           pointer-events-auto animate-text-flicker
           ${className}
         `} 
