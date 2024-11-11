6a15250e432c638ffdec954d05850b31d795ae22
Axiom <101472184+Axiiomatic@users.noreply.github.com>
2024-11-11 14:22:44 -0500

    fixed gui screen formatting

--- src/components/screens/GUIScreen.tsx
+++ src/components/screens/GUIScreen.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import {
   CenterWrapper
 } from "@/components/wrappers";
 import "@/styles/gui.css";
 
-export const GUIScreen = () => {
+interface Props {
+    children?: ReactNode
+}
+
+export const GUIScreen = ({ children } : Props) => {
     return (
-    <div>
-        <div className="gui-background" />
-        <CenterWrapper>
-            GUI (Work in Progress)
-        </CenterWrapper>
+    <div className="h-full overflow-hidden">
+      <div className="gui-background" />
+      <div className="h-full z-[15] relative">
+        <div className="overflow-x-hidden overflow-y-auto h-full flex flex-col relative z-[15]">
+          {children}
+        </div>
+      </div>
     </div>
-    );
+    )
 };
   
 export default GUIScreen;
