09710c5b7aa3acbf6986184af867f88ba9d842b6
Axiom <axiiomatic@users.noreply.github.com>
2024-11-12 18:11:50 -0400

    minor changes to gui background

--- src/components/screens/gui/GUIScreen.tsx
+++ src/components/screens/gui/GUIScreen.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const GUIScreen = ({ children } : Props) => {
     return (
     <div className="h-full overflow-hidden">
-      <div className="gui-background" />
+      <div className="gui-background absolute top-0 left-0 w-full h-full " />
       <div className="h-full z-[15] relative">
         <div className="overflow-x-hidden overflow-y-auto h-full flex flex-col relative z-[15]">
           {children}
